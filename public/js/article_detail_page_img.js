5d403bc32e603b2e61459fc18da19025c73b96b9
ZhaoYi <yzzhao@thoughtworks.com>
2015-12-11 16:45:29 +0800

    [zhaoyi] update article detail page for video

--- public/js/article_detail_page_img.js
+++ public/js/article_detail_page_img.js
@@ -8,14 +8,29 @@ angular.module('articleDetailPageModel',['ngRoute']).
     	 	.success(function(e){
     	 		console.log('get course ',e);
     	 		$scope.course = e;
-                $scope.showImage = ($scope.course.titleImagePath !== undefined) 
-                        && ($scope.course.videoPath === undefined);
-                $scope.showVideo = ! $scope.showImage;
+                
+                
                 console.log('show video '+$scope.showVideo);
                 console.log('show image '+$scope.showImage);
                 console.log('aaa '+$scope.course.titleImagePath !== undefined);
                 console.log($scope.course.videoPath === undefined);
     	 		document.getElementById('courseintro').innerHTML=$scope.course.content;
+
+                // $http.get('http://'+$location.host()+":8080/education/zaozao/course/file/"+id+"/"+$scope.course.videoPath)
+                // .success(function(e){
+                //     console.log('read '+e);
+                    //$scope.course.videoUrl='data:image/jpeg;base64,' +btoa(e);
+                    $scope.showVideo = ($scope.course.titleImageUrl === undefined) 
+                        || ($scope.course.videoUrl !== undefined);
+                // });
+
+                // $http.get('http://'+$location.host()+":8080/education/zaozao/course/file/"+id+"/"+$scope.course.titleImagePath)
+                // .success(function(e){
+                    //$scope.course.titleImageUrl='data:image/jpeg;base64,' +e;
+                    $scope.showImage = ($scope.course.titleImageUrl !== undefined) 
+                        && ($scope.course.videoUrl === undefined);
+                // });
+
     	 	}).error(function(e){
 
     	 	});
